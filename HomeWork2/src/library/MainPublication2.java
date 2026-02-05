916bc8be4185d2cded0956b227fe67d75cfd8b09
yoav31 <yoav31yv@gmail.com>
2026-02-05 23:35:40 +0200

    Refactor MainPublication2 for publication sorting

--- HomeWork2/src/library/MainPublication2.java
+++ HomeWork2/src/library/MainPublication2.java
@@ -1,5 +1,4 @@
-// Assignment: 2
-// Author: Yoav Haviv Vaknin, ID: 208323261
+
 package library;
 
 public class MainPublication2 {
@@ -102,4 +101,5 @@ public class MainPublication2 {
         }
     }
 }
+
 
