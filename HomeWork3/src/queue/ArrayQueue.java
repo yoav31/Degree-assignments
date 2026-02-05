209a6219dfcf18fbde5aefd98fa4dd9df707814c
yoav31 <yoav31yv@gmail.com>
2026-02-05 23:36:14 +0200

    Update ArrayQueue.java

--- HomeWork3/src/queue/ArrayQueue.java
+++ HomeWork3/src/queue/ArrayQueue.java
@@ -1,5 +1,4 @@
-// Assignment: 3
-// Author: Yoav Haviv Vaknin, ID: 208323261
+
 package queue;
 
 public class ArrayQueue implements Queue {
@@ -123,3 +122,4 @@ public class ArrayQueue implements Queue {
     }
 
 }
+
