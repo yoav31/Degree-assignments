d0aa92aaacbb3e8e34645b8cc681e1900d87e8aa
yoav31 <yoav31yv@gmail.com>
2026-02-08 10:56:24 +0200

    Update MathTeacher.java

--- HomeWork3/src/school/MathTeacher.java
+++ HomeWork3/src/school/MathTeacher.java
@@ -1,5 +1,4 @@
-// Assignment: 3
-// Author: Yoav Haviv Vaknin, ID: 208323261
+
 package school;
 
 import java.util.Scanner;
@@ -79,4 +78,5 @@ public class MathTeacher extends Teacher{
                 '}';
     }
 }
+
 
