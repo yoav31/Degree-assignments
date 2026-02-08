9152a6687f5792d12eec0b0b24d5023999894e9a
yoav31 <yoav31yv@gmail.com>
2026-02-08 10:55:33 +0200

    Update ScienceTeacher.java

--- HomeWork3/src/school/ScienceTeacher.java
+++ HomeWork3/src/school/ScienceTeacher.java
@@ -1,5 +1,4 @@
-// Assignment: 3
-// Author: Yoav Haviv Vaknin, ID: 208323261
+
 package school;
 import java.util.Scanner;
 
@@ -81,3 +80,4 @@ public class ScienceTeacher extends Teacher{
                 '}';
     }
 }
+
